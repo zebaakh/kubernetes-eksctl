cc6a28818a9b4e1f3c4f74041c3d2327b5e3b783
zeba akhtari <zeba.akhtari@abzooba.com>
2024-03-06 17:31:57 +0530

    push files

--- workstation.sh
+++ workstation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -6,22 +7,19 @@ Y="\e[33m"
 USERID=$(id -u)
 
 LOGSDIR=/tmp
-# /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$(basename "$0")
 DATE=$(date +%F)
 LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
 echo -e "$Y This script runs on CentOS 8 $N"
 
-if [[ "$USERID" -ne 0 ]];
-then
+if [ "$USERID" -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
 
-VALIDATE(){
-    if [ $1 -ne 0 ];
-    then
+VALIDATE() {
+    if [ "$1" -ne 0 ]; then
         echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
@@ -29,51 +27,42 @@ VALIDATE(){
     fi
 }
 
-yum install -y yum-utils &>>$LOGFILE
-
+yum install -y yum-utils &>> "$LOGFILE"
 VALIDATE $? "yum-utils package installed"
 
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &>>$LOGFILE
-
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &>> "$LOGFILE"
 VALIDATE $? "Docker Repo added"
 
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y &>>$LOGFILE
-
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y &>> "$LOGFILE"
 VALIDATE $? "Docker components are installed"
 
-systemctl start docker &>>$LOGFILE
-
+systemctl start docker &>> "$LOGFILE"
 VALIDATE $? "Docker Started"
 
-systemctl enable docker &>>$LOGFILE
-
+systemctl enable docker &>> "$LOGFILE"
 VALIDATE $? "Docker Enabled"
 
-usermod -aG docker centos &>>$LOGFILE
-
+usermod -aG docker centos &>> "$LOGFILE"
 VALIDATE $? "centos user added to docker group"
 
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp &>>$LOGFILE
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp &>> "$LOGFILE"
 VALIDATE $? "Downloaded eksctl command"
-chmod +x /tmp/eksctl &>>$LOGFILE
-VALIDATE $?  "Added execute permissions to eksctl"
-mv /tmp/eksctl /usr/local/bin &>>$LOGFILE
-VALIDATE $? "moved eksctl to bin folder"
+chmod +x /tmp/eksctl &>> "$LOGFILE"
+VALIDATE $? "Added execute permissions to eksctl"
+mv /tmp/eksctl /usr/local/bin &>> "$LOGFILE"
+VALIDATE $? "Moved eksctl to bin folder"
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &>>$LOGFILE
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &>>$LOGFILE
-
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &>> "$LOGFILE"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &>> "$LOGFILE"
 VALIDATE $? "kubectl installed"
 
-git clone https://github.com/ahmetb/kubectx /opt/kubectx &>>$LOGFILE
-ln -s /opt/kubectx/kubens /usr/local/bin/kubens &>>$LOGFILE
-
+git clone https://github.com/ahmetb/kubectx /opt/kubectx &>> "$LOGFILE"
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens &>> "$LOGFILE"
 VALIDATE $? "kubens Installation"
 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 &>> "$LOGFILE"
 chmod 700 get_helm.sh
-./get_helm.sh
-
+./get_helm.sh &>> "$LOGFILE"
 VALIDATE $? "Helm Installation"
 
 echo -e "$R Please logout and login again $N"
